574c2f0c0b3d011896718a6ba03421e334c5813d
Jessica Lewinter <jessicalewinter@gmail.com>
2022-10-29 18:28:37 -0300

    fix: add #if available compiler flag

--- Sources/Utilities/UIKit/Extensions/UIView+setToEdges.swift
+++ Sources/Utilities/UIKit/Extensions/UIView+setToEdges.swift
@@ -4,12 +4,22 @@ import UIKit
 
 extension UIView {
     public func setToEdges(_ view: UIView, padding: CGFloat = 0) {
-        NSLayoutConstraint.activate([
-            view.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: padding),
-            view.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
-            view.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -padding),
-            view.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: padding)
-        ])
+        if #available(iOS 11.0, *) {
+            NSLayoutConstraint.activate([
+                view.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: padding),
+                view.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
+                view.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -padding),
+                view.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: padding)
+            ])
+        } else {
+            // Fallback on earlier versions
+            NSLayoutConstraint.activate([
+                view.topAnchor.constraint(equalTo: self.topAnchor, constant: padding),
+                view.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+                view.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -padding),
+                view.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding)
+                ])
+        }
     }
 }
 #endif
